aef13db76a2585e655b7843b311f67e7f3cf15e8
charles <charles.bernard@cri-paris.org>
2021-06-24 08:54:41 +0200

    test if key in dict rather than in list to speed up execution

--- python/handle_fasta.py
+++ python/handle_fasta.py
@@ -79,16 +79,23 @@ def explicitize_duplicates(in_faa, out_dir, duplicates):
             list_protein_ids.append(protein_id)
     outfile.close()
     shutil.move(out_faa, in_faa)
-    return list_protein_ids      
+    return list_protein_ids
+
+
+def list_to_dict(l):
+    d = dict()
+    for element in l:
+        d[element] = True
+    return d    
 
 
 def filter_by_adjacency(propeptides_ids, receptors_ids, protein_dict, position_dict):
     if len(propeptides_ids) < len(receptors_ids):
         list_a = propeptides_ids
-        list_b = receptors_ids
+        dict_b = list_to_dict(receptors_ids)
     else:
         list_a = receptors_ids
-        list_b = propeptides_ids
+        dict_b = list_to_dict(propeptides_ids)
     filt_list_a = list()
     filt_list_b = list()
     unique_orfs = dict()
@@ -104,7 +111,7 @@ def filter_by_adjacency(propeptides_ids, receptors_ids, protein_dict, position_d
             flanking_orfs.append(position_dict[(genomic_accession, position+1)]['protein_id'])
         # test if any flanking protein is in the longest list
         for flanking_orf in flanking_orfs:
-            if flanking_orf in list_b:
+            if flanking_orf in dict_b:
                 if not flanking_orf in unique_orfs:
                     filt_list_b.append(flanking_orf)
                     unique_orfs[flanking_orf] = True
@@ -117,7 +124,8 @@ def filter_by_adjacency(propeptides_ids, receptors_ids, protein_dict, position_d
         return filt_list_b, filt_list_a
     
 
-def subset_by_id(in_faa, out_faa, ids):
+def subset_by_id(in_faa, out_faa, proteins):
+    ids = list_to_dict(proteins)
     k = 0
     # create a dictionary of visited proteins in order to skip reading fasta if all query proteins have been extracted from the faa 
     is_visited = dict()
